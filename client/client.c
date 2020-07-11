e1b940080f30c0fd7a2e0eab034b7829b8f3aeec
LordParmal <744240532@qq.com>
2020-07-11 12:52:16 +0800

    b

---
+++ client/client.c
@@ -0,0 +1,167 @@
+/*************************************************************************
+	> File Name: client.c
+	> Author: suyelu 
+	> Mail: suyelu@126.com
+	> Created Time: Wed 08 Jul 2020 04:32:12 PM CST
+ ************************************************************************/
+
+#include "head.h"
+
+int server_port = 0;
+char server_ip[20] = {0};
+char *conf = "./football.conf";
+int sockfd = -1;
+
+void *do_recv () {
+    struct ChatMsg msg;
+    while (1) {
+        bzero(&msg, sizeof(msg));
+        recv(sockfd, (void *)&msg, sizeof(msg), 0);
+        
+        if (msg.type & CHAT_WALL) {
+            printf(L_PINK" <%s> ~ "NONE"%s\n", msg.name, msg.msg);
+        } else if (msg.type & CHAT_MSG){
+            printf("%s\n", msg.msg);
+        } else if (msg.type & CHAT_FUNC) {
+            printf("%s\n", msg.msg);
+        } else if (msg.type & CHAT_FIN){
+            printf(GREEN"通知:"NONE"\n%s\n", msg.msg);
+            exit(1);
+        } else if (msg.type & CHAT_SYS) {
+            printf(L_GREEN"公告:"NONE"%s\n", msg.msg);
+        }
+    }
+}
+
+void logout(int signum) {
+    struct ChatMsg msg;
+    bzero(&msg, sizeof(msg));
+    msg.type = CHAT_FIN;
+    send(sockfd, (void *)&msg, sizeof(msg), 0);
+    close(sockfd);
+    printf(L_PINK"byebye"NONE"\n");
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    int opt;
+    struct LogRequest request;
+    struct LogResponse response;
+    bzero(&request, sizeof(request));
+    bzero(&response, sizeof(response));
+    while ((opt = getopt(argc, argv, "h:p:t:m:n:")) != -1) {
+        switch (opt) {
+            case 't':
+                request.team = atoi(optarg);
+                break;
+            case 'h':
+                strcpy(server_ip, optarg);
+                break;
+            case 'p':
+                server_port = atoi(optarg);
+                break;
+            case 'm':
+                strcpy(request.msg, optarg);
+                break;
+            case 'n':
+                strcpy(request.name, optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage : %s [-hptmn]!\n", argv[0]);
+                exit(1);
+        }
+    }
+    
+
+    if (!server_port) server_port = atoi(get_conf_value(conf, "SERVERPORT"));
+    if (!request.team) request.team = atoi(get_conf_value(conf, "TEAM"));
+    if (!strlen(server_ip)) strcpy(server_ip, get_conf_value(conf, "SERVERIP"));
+    if (!strlen(request.name)) strcpy(request.name, get_conf_value(conf, "NAME"));
+    if (!strlen(request.msg)) strcpy(request.msg, get_conf_value(conf, "LOGMSG"));
+
+
+    DBG("<"GREEN"Conf Show"NONE"> : server_ip = %s, port = %d, team = %s, name = %s\n%s",\
+        server_ip, server_port, request.team ? "BLUE": "RED", request.name, request.msg);
+
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(server_port);
+    server.sin_addr.s_addr = inet_addr(server_ip);
+
+    socklen_t len = sizeof(server);
+
+    if ((sockfd = socket_udp()) < 0) {
+        perror("socket_udp()");
+        exit(1);
+    }
+    
+    pthread_t recv_t;          
+    int err = pthread_create(&recv_t, NULL, do_recv, NULL);
+    
+    if (err < 0) {
+        perror("create pthread fail!\n");
+        exit(1);
+    }
+
+    sendto(sockfd, (void *)&request, sizeof(request), 0, (struct sockaddr *)&server, len);
+
+    fd_set rfds;
+    FD_ZERO(&rfds);
+    FD_SET(sockfd, &rfds);
+    struct timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+    DBG("<"PINK"ADD rfds"NONE"> : set %d in rfds.\n", sockfd);
+    if (select(sockfd + 1 , &rfds, NULL, NULL, &tv) <= 0) {
+        perror("response error!");
+        exit(1);
+    }
+    if (FD_ISSET(sockfd, &rfds)) {
+        recvfrom(sockfd, (void *)&response, sizeof(response), 0,(struct sockaddr *)&server, &len);
+        if ((!(strlen(response.msg) < 512 && strlen(response.msg) > 0)) || response.type == 1) {
+            fprintf(stderr, "server refused! : %s\n", response.msg);
+            exit(1);
+        }
+        
+    }
+    if (connect(sockfd, (const struct sockaddr*)&server, len) < 0) {
+            perror("connect fail");
+            exit(1);
+        }
+
+    DBG(GREEN"connect successfully"NONE"<%s> : %s\n", server_ip, request.msg);
+    
+    //char buff[512] = {0};
+    //sprintf(buff, "Does it work?");
+    //send(sockfd, buff, strlen(buff), 0);
+    //recv(sockfd, buff, sizeof(buff), 0);
+    //DBG(RED"Server Info"NONE" : %s\n",buff);
+    signal(SIGINT, logout);
+
+ 
+    while(1) {
+        
+        usleep(100000);
+        struct ChatMsg msg;
+        bzero(&msg, sizeof(msg));
+        msg.type = CHAT_WALL;
+        strcpy(msg.name, request.name);
+        printf(L_RED"说点什么吧 ："NONE);
+        scanf("%[^\n]s", msg.msg);
+        getchar();
+        if (!strlen(msg.msg)) {
+            continue;
+        }
+        if(msg.msg[0] == '@') {
+            msg.type = CHAT_MSG;
+        }
+        if(msg.msg[0] == '#') {
+            msg.type = CHAT_FUNC;
+        }
+        send(sockfd, (void *)&msg, sizeof(msg), 0);
+
+    }
+
+
+    return 0;
+}
---
+++ common/thread_pool.c
@@ -0,0 +1,118 @@
+/*************************************************************************
+	> File Name: thread_pool.c
+	> Author: suyelu 
+	> Mail: suyelu@126.com
+	> Created Time: Thu 09 Jul 2020 02:50:28 PM CST
+ ************************************************************************/
+
+#include "head.h"
+extern int repollfd, bepollfd;
+
+extern struct User *rteam;
+extern struct User *bteam;
+
+void do_work(struct User *user){
+    struct ChatMsg msg;
+    bzero(&msg, sizeof(msg));
+    recv(user->fd, (void *)&msg, sizeof(msg), 0);
+
+    if (msg.type & CHAT_WALL) {
+        printf(L_PINK"<%s> ~ "NONE"%s \n", user->name, msg.msg);
+        send_all(&msg);
+    } else if (msg.type & CHAT_MSG) {
+        int i;
+        char receiver[20] = {0};
+        struct ChatMsg send_msg;
+        bzero(&send_msg, sizeof(send_msg));
+
+        for (i = 0; msg.msg[i + 1] != ' '; i++) {
+            receiver[i] = msg.msg[i + 1]; 
+        }
+
+        struct User *recv;
+
+        if ((recv = find_receiver(receiver)) == NULL) {
+            sprintf(send_msg.msg, L_GREEN"私聊格式错误，或者对方不在线"NONE);
+            strcpy(send_msg.name, msg.name);
+            send_msg.type = CHAT_MSG;
+            send_one(&send_msg, user);
+        } else {
+            char temp[1024] = {0};
+
+            strcpy(send_msg.name, msg.name);
+            send_msg.type = CHAT_MSG;
+            strncpy(temp, msg.msg + (i + 1), strlen(msg.msg + (i + 1)));
+            sprintf(send_msg.msg, YELLOW"<%s> --> <%s> $"BLUE"  %s"NONE, user->name, recv->name, temp);
+
+            send_one(&send_msg, user);
+            send_one(&send_msg, recv);
+            printf("%s\n", send_msg.msg);
+        }
+    } else if (msg.type & CHAT_FIN) {
+        user->online = 0;
+        int epollfd = user->team ? bepollfd : repollfd;
+        del_event(epollfd, user->fd);
+        printf(GREEN"Server Info"NONE" : %s logout\n", user->name);
+        struct ChatMsg send_msg;
+        bzero(&send_msg, sizeof(send_msg));
+        send_msg.type = CHAT_SYS;
+        sprintf(send_msg.msg, L_YELLOW"%s下线了"NONE, user->name);
+        send_all(&send_msg);
+
+        close(user->fd);
+    } else if (msg.type & CHAT_FUNC) {
+        switch(msg.msg[1]) {
+            case '1' : show_members(user); break;
+            default : break;
+        }
+    }
+}
+
+
+void task_queue_init(struct task_queue *taskQueue, int sum, int epollfd) {
+    taskQueue->sum = sum;
+    taskQueue->epollfd = epollfd;
+    taskQueue->team = calloc(sum, sizeof(void *));
+    taskQueue->head = taskQueue->tail = 0;
+    pthread_mutex_init(&taskQueue->mutex, NULL);
+    pthread_cond_init(&taskQueue->cond, NULL);
+}
+
+void task_queue_push(struct task_queue *taskQueue, struct User *user) {
+    pthread_mutex_lock(&taskQueue->mutex);
+    taskQueue->team[taskQueue->tail] = user;
+    DBG(L_GREEN"Thread Pool"NONE" : Task push %s\n", user->name);
+    if (++taskQueue->tail == taskQueue->sum) {
+        DBG(L_GREEN"Thread Pool"NONE" : Task Queue End\n");
+        taskQueue->tail = 0;
+    }
+    pthread_cond_signal(&taskQueue->cond);
+    pthread_mutex_unlock(&taskQueue->mutex);
+}
+
+
+struct User *task_queue_pop(struct task_queue *taskQueue) {
+    pthread_mutex_lock(&taskQueue->mutex);
+    while (taskQueue->tail == taskQueue->head) {
+        DBG(L_GREEN"Thread Pool"NONE" : Task Queue Empty, Waiting For Task\n");
+        pthread_cond_wait(&taskQueue->cond, &taskQueue->mutex);
+    }
+    struct User *user = taskQueue->team[taskQueue->head];
+    DBG(L_GREEN"Thread Pool"NONE" : Task Pop %s\n", user->name);
+    if (++taskQueue->head == taskQueue->sum) {
+        DBG(L_GREEN"Thread Pool"NONE" : Task Queue End\n");
+        taskQueue->head = 0;
+    }
+    pthread_mutex_unlock(&taskQueue->mutex);
+    return user;
+}
+
+void *thread_run(void *arg) {
+    pthread_detach(pthread_self());
+    struct task_queue *taskQueue = (struct task_queue *)arg;
+    while (1) {
+        struct User *user = task_queue_pop(taskQueue);
+        do_work(user);
+    }
+}
+
